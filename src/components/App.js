dcce3eb0422ec8efb32439b56cca2d93a5833bc6
Michael Chung <mikechung89@gmail.com>
2022-03-22 01:29:43 -0400

    commit tree 81f7cc2aeee4a03a6faba8287f37f14c9d21592a

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 import Filters from "./Filters";
 import PetBrowser from "./PetBrowser";
@@ -6,6 +6,27 @@ import PetBrowser from "./PetBrowser";
 function App() {
   const [pets, setPets] = useState([]);
   const [filters, setFilters] = useState({ type: "all" });
+  const [type, onChangeType] = useState('all')
+
+  useEffect(() => {
+    const fetcher = () => {
+      let path = '';
+    filters.type === 'all' ? path = '/pets' : path = `/pets?type=${filters.type}`;
+
+    fetch(`http://localhost:3001${path}`)
+    .then(resp => resp.json())
+    .then(data => setPets(data))}
+
+    fetcher()
+
+    // cleanup
+    return () => {}
+  },[filters])
+  
+  function onFindPetsClick () {
+    console.log(type)
+    setFilters({type: type})
+  }
 
   return (
     <div className="ui container">
@@ -15,10 +36,13 @@ function App() {
       <div className="ui container">
         <div className="ui grid">
           <div className="four wide column">
-            <Filters />
+            <Filters
+              onChangeType={onChangeType}
+              onFindPetsClick={onFindPetsClick}
+            />
           </div>
           <div className="twelve wide column">
-            <PetBrowser />
+            <PetBrowser pets={pets} setPets={setPets} />
           </div>
         </div>
       </div>
