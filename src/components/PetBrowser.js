5ade5a8f6c57ad153a1b1a96d8afd14f283aef7a
Michael Chung <mikechung89@gmail.com>
2022-03-22 01:30:07 -0400

    delivered minus one

--- src/components/PetBrowser.js
+++ src/components/PetBrowser.js
@@ -1,9 +1,23 @@
 import React from "react";
-
 import Pet from "./Pet";
 
-function PetBrowser() {
-  return <div className="ui cards">PET COMPONENT SHOULD GO HERE</div>;
+function PetBrowser({ pets, setPets }) {
+
+  function onAdoptPet (id) {
+    const remPets = pets.map(pet => pet.id !== id ? pet : {...pet, isAdopted: true})
+    setPets(remPets)
+  }
+
+  const petRender = pets.map(pet => {
+  return (
+    <Pet
+      onAdoptPet={onAdoptPet}
+      key={pet.id}
+      pet={pet}
+    />)
+  })
+
+  return <div className="ui cards">{petRender}</div>;
 }
 
 export default PetBrowser;
